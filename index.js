e5ee099114ac9e2f1e511f07cffcac8fc1c0f5d4
AndriFP <113114423+andrifp@users.noreply.github.com>
2023-05-20 21:48:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,11 +17,10 @@ const app = express();
 
 //konfigurasi koneksi
 const conn = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  host: 'sql12.freemysqlhosting.net',
+  user: 'sql12619671',
+  password: 'FT9Ysirkiu',
+  database: 'db_lingkom2'
 });
 
 //connect ke database
